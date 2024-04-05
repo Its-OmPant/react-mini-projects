a1c96c91bc747ea12d6edc4157128561e8c35d3a
Om Pant <ompant992@gmail.com>
2024-04-05 15:38:31 +0530

    updated counter ui

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -17,24 +17,24 @@ function CounterApp() {
 	};
 
 	return (
-		<div className="bg-teal-200 w-[80%] m-auto p-8 py-16 rounded-2xl text-center shadow-md sm:w-1/2  md:w-1/3 ">
+		<div className="bg-teal-200 w-[80%] m-auto my-10 p-8 py-16 rounded-2xl text-center shadow-md sm:w-1/2  md:w-1/3 ">
 			<h3 className="text-xl my-2">Counter App</h3>
 			<h1 className="text-3xl my-6 text-teal-950 font-bold sm:text-4xl lg:text-5xl">
 				Counter : {counter}
 			</h1>
 			<div className="">
 				<button
-					className="bg-teal-700 text-white rounded-xl p-2 m-2 w-32 text-lg"
+					className="bg-teal-800 text-white rounded-xl p-2 m-2 w-32 text-lg"
 					onClick={decrement}>
 					Decrement
 				</button>
 				<button
-					className="bg-teal-700 text-white rounded-xl p-2 m-2 w-32 text-lg"
+					className="bg-teal-800 text-white rounded-xl p-2 m-2 w-32 text-lg"
 					onClick={reset}>
 					Reset
 				</button>
 				<button
-					className="bg-teal-700 text-white rounded-xl p-2 m-2 w-32 text-lg"
+					className="bg-teal-800 text-white rounded-xl p-2 m-2 w-32 text-lg"
 					onClick={increment}>
 					Increment
 				</button>
